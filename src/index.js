fea8964a4c435b623af0563035e8316e837c525f
Luke Opperman <luke@opperman.net>
2018-11-06 12:28:49 -0500

    Move success trace output within try block

--- src/index.js
+++ src/index.js
@@ -551,10 +551,10 @@ class Searcher extends NxusModule {
     let SD = await this._getSearchDocument(model)
     try {
       await SD.create(doc)
+      this.log.trace('Search document created', model, doc.id)
     } catch(e) {
       this.log.trace("Search create error", model, doc.id, e)
     }
-    this.log.trace('Search document created', model, doc.id)
   }
 
   async _handleDestroy(model, doc) {
@@ -562,10 +562,10 @@ class Searcher extends NxusModule {
     let SD = await this._getSearchDocument(model)
     try {
       await SD.destroy().where(doc.id)
+      this.log.trace('Search document deleted', model, doc.id)
     } catch(e) {
       this.log.trace("Search destroy error", model, doc.id, e)
     }
-    this.log.trace('Search document deleted', model, doc.id)
   }
 
   async _handleUpdate(model, doc) {
@@ -574,10 +574,10 @@ class Searcher extends NxusModule {
     let SD = await this._getSearchDocument(model)
     try {
       await SD.update(doc.id, doc)
+      this.log.trace('Search document updated', model, doc.id)
     } catch(e) {
       this.log.trace("Search update error", model, doc.id, e)
     }
-    this.log.trace('Search document updated', model, doc.id)
   }
 }
 
