676522fb55079586ffc63880dca03e926140fc21
Luke Opperman <luke@opperman.net>
2019-04-15 13:38:27 -0400

    Switch to nxus-tester-jest for tests

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
-/* 
-* @Author: Mike Reich
-* @Date:   2016-02-13 08:59:44
-* @Last Modified 2016-05-20
-*/
+/**
+ * @jest-environment node
+ */
+
+/* globals beforeEach: false, describe: false, it: false, expect: false */
 
 'use strict';
 
@@ -14,41 +14,41 @@ describe("Searcher", () => {
  
   describe("Load", () => {
     it("should not be null", () => {
-      Searcher.should.not.be.null
-      searcher.should.not.be.null
+      expect(Searcher).not.toBeNull()
+      expect(searcher).not.toBeNull()
     })
 
     it("should be instantiated", () => {
       module = new Searcher();
-      module.should.not.be.null;
+      expect(module).not.toBeNull()
     });
 
     it("should have searchable", () => {
-      module.searchable.should.not.be.null
+      expect(module).toHaveProperty('searchable')
     })
   });
 
   describe("Indexes", () => {
-    before(() => {
+    beforeEach(() => {
       module = new Searcher()
     })
     it("should register the default document", () => {
-      module._searchDocuments.should.have.property('', 'searchdocument')
+      expect(module._searchDocuments).toHaveProperty('', 'searchdocument')
     })
     it("createSearchDocument should register new model", () => {
       module._createSearchDocument('second')
-      module._searchDocuments.should.have.property('second', 'searchdocument-second')
+      expect(module._searchDocuments).toHaveProperty('second', 'searchdocument-second')
     })
   })
   
   describe("Query", () => {
-    before(() => {
+    beforeEach(() => {
       module = new Searcher()
       module.modelConfig['test'] = {model: 'test', fields: ['name', 'title']}
     })
     it("should take a text field", () => {
       let q = module._buildQuery("test", "text")
-      q.should.eql({
+      expect(q).toEqual({
         query: {
           bool: {
             minimum_should_match: 1,
@@ -65,7 +65,7 @@ describe("Searcher", () => {
     })
     it("should take a text field and extra filters", () => {
       let q = module._buildQuery("test", "text", {filters: [{term: {type: 'value'}}]})
-      q.should.eql({
+      expect(q).toEqual({
         query: {
           bool: {
             minimum_should_match: 1,
@@ -83,7 +83,7 @@ describe("Searcher", () => {
     })
     it("should take a text field and sort option", () => {
       let q = module._buildQuery("test", "text", {sort: ["name", {"title": "desc"}]})
-      q.should.eql({
+      expect(q).toEqual({
         query: {
           bool: {
             minimum_should_match: 1,
@@ -101,7 +101,7 @@ describe("Searcher", () => {
     })
     it("should take an alternate match query type", () => {
       let q = module._buildQuery("test", "text", {match_query: 'prefix'})
-      q.should.eql({
+      expect(q).toEqual({
         query: {
           bool: {
             minimum_should_match: 1,
@@ -118,7 +118,7 @@ describe("Searcher", () => {
     })
     it("should take a minimum_should_match ovveride", () => {
       let q = module._buildQuery("test", "text", {minimum_should_match: 0})
-      q.should.eql({
+      expect(q).toEqual({
         query: {
           bool: {
             minimum_should_match: 0,
@@ -135,7 +135,7 @@ describe("Searcher", () => {
     })
     it("should take an alternate list of fields", () => {
       let q = module._buildQuery("test", "text", {fields: ["title"]})
-      q.should.eql({
+      expect(q).toEqual({
         query: {
           bool: {
             minimum_should_match: 1,
@@ -154,7 +154,7 @@ describe("Searcher", () => {
         operator: "and",
         minimum_should_match: "75%"
       }})
-      q.should.eql({
+      expect(q).toEqual({
         query: {
           bool: {
             minimum_should_match: 1,
